c311819c3854046cfaca027b7e1c11ab9e2279a3
Théo <theo_perso@live.fr>
2025-07-23 14:30:30 +0200

    better syntax

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const authenticate = require('../middleware/authMiddleware');
 const movieController = require("../controllers/movieController");
-const authenticate = require("../middleware/authMiddleware");
+
+
 
 router.post("/add", authenticate, movieController.addMovie);
 router.get("/", authenticate, movieController.getUserMovies);
-router.put("/:id", authenticate, updateMovie);
+router.put("/:id", authenticate, movieController.updateMovie);
 router.delete("/:id", authenticate, movieController.deleteMovie);
 
 module.exports = router;
