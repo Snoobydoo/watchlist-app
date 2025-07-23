7fd9ecd0c1d3c69dbcc5dbd3a976c915517044db
Théo <theo_perso@live.fr>
2025-07-23 14:21:03 +0200

    add update route

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -5,6 +5,7 @@ const authenticate = require("../middleware/authMiddleware");
 
 router.post("/add", authenticate, movieController.addMovie);
 router.get("/", authenticate, movieController.getUserMovies);
+router.put("/:id", authenticate, updateMovie);
 router.delete("/:id", authenticate, movieController.deleteMovie);
 
 module.exports = router;
