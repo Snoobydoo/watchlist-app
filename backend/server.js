ac53af7a029a47982e2a5fd17890f9dd1c07881d
Théo <theo_perso@live.fr>
2025-07-23 16:02:05 +0200

    modify quotes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-const authRoutes = require('./routes/authRoutes');
-app.use('/user', authRoutes);
+const authRoutes = require("./routes/authRoutes");
+app.use("/user", authRoutes);
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API Watchlist opérationnelle');
+app.get("/", (req, res) => {
+  res.send("API Watchlist opérationnelle");
 });
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('Connecté à MongoDB');
-    app.listen(5000, () => console.log('Serveur sur http://localhost:5000'));
+    console.log("Connecté à MongoDB");
+    app.listen(5000, () => console.log("Serveur sur http://localhost:5000"));
   })
-  .catch((err) => console.error('Erreur MongoDB :', err));
+  .catch((err) => console.error("Erreur MongoDB :", err));
