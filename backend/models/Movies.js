2fc5ba47178386c33cf8317ccc92466c4c85c27d
Théo <theo_perso@live.fr>
2025-07-23 11:35:04 +0200

    add Movie model

---
+++ backend/models/Movies.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  imdbID: { type: String, required: true },
+  poster: { type: String },
+  status: {
+    type: String,
+    enum: ["à voir", "en cours", "vu"],
+    default: "à voir"
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Movie", movieSchema);
