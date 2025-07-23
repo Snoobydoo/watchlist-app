dc89cc209456721b8ea72cc3e47ef19163e90c15
Théo <theo_perso@live.fr>
2025-07-23 11:55:31 +0200

    add movie controller

---
+++ backend/controllers/movieController.js
@@ -0,0 +1,41 @@
+const Movie = require("../models/Movies");
+
+exports.addMovie = async (req, res) => {
+  const { title, imdbID, poster, status, rating } = req.body;
+
+  try {
+    const newMovie = new Movie({
+      title,
+      imdbID,
+      poster,
+      status,
+      rating,
+      user: req.user.id,
+    });
+
+    await newMovie.save();
+    res.status(201).json(newMovie);
+  } catch (error) {
+    res.status(500).json({ message: "Erreur lors de l'ajout du film." });
+  }
+};
+
+exports.getUserMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find({ user: req.user.id });
+    res.status(200).json(movies);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la récupératiion des films." });
+  }
+};
+
+exports.deleteMovie = async (req, res) => {
+  try {
+    await Movie.findOneAndDelete({ _id: req.params.id, user: req.user.id });
+    res.status(200).json({ message: "Film supprimé." });
+  } catch (error) {
+    res.status(500).json({ message: "Erreur lors de la suppression du film." });
+  }
+};
