7503ad9933509912e94111de693b8d5d5161c73f
Théo <theo_perso@live.fr>
2025-07-28 15:31:23 +0200

    add temp content in pages

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,8 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>Page d'inscription</h1>
+      <p>Veuillez remplir le formulaire pour vous inscrire.</p>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Watchlist.jsx
@@ -0,0 +1,8 @@
+export default function Watchlist() {
+  return (
+    <div>
+      <h1>Page de la liste de films</h1>
+      <p>Voici votre liste de films à surveiller.</p>
+    </div>
+  );
+}
