a86ae5c47d98b2d65373a33dfd4180fee1adfb0f
Théo <theo_perso@live.fr>
2025-07-28 14:30:47 +0200

    add basic routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Watchlist from "./pages/Watchlist";
 
 function App() {
-  return <h1>Boulgour</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
